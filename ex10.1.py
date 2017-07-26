3e3f5ff505b07c033c4b9163e3df7015155b9de3
RachyJ <RachyJ@users.noreply.github.com>
2017-07-26 21:50:09 +0800

    Add files via upload
    from mac machine

---
+++ ex10.1.py
@@ -0,0 +1,6 @@
+while True:
+#	for i in ["/","-","|","\\","|"]:
+#		print "%s\r" % i,
+
+	for i in ['/','-','|','\\','|']:
+		print '%r\r' % i,
---
+++ ex11.py
@@ -0,0 +1,17 @@
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weigh?",
+# weight = raw_input()
+
+# print "So you are %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "Where are you from?"
+city = raw_input()
+print "How do you like it?"
+howlike = raw_input()
+print "How long have you been there?"
+stay = raw_input()
+
+print "So you are from %r city, you like it %r, you've stayed there %r." % (city, howlike, stay)
